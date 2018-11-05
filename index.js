da546b62f20aba9df151edb6c8dc067b3959758d
Joe Bryan <joemfb@gmail.com>
2018-11-05 17:10:17 -0500

    refactors arvo tests into modular actions

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 'use strict';
 
-var runner = require('./lib/runner.js')
+var runner  = require('./lib/runner.js')
+var actions = require('./lib/actions.js')
 
 module.exports = {
-  Urbit: runner.Urbit,
-  ERROR: runner.ERROR
+  Urbit:   runner.Urbit,
+  ERROR:   runner.ERROR,
+  actions: actions
 }
