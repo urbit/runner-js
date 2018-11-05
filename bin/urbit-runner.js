1f172cd4a561155a81075c86221f78657a16a799
Joe Bryan <joemfb@gmail.com>
2018-11-05 17:03:53 -0500

    refactors compiled js, restores comments

--- bin/urbit-runner.js
+++ bin/urbit-runner.js
@@ -1,18 +1,36 @@
-var split, through, ref$, Urbit, ERROR, urbit, slice$ = [].slice;
-split = require('split');
-through = require('promise-streams').through;
-ref$ = require('../index.js'), Urbit = ref$.Urbit, ERROR = ref$.ERROR;
-urbit = new Urbit(slice$.call(process.argv, 3));
-urbit.expect(ERROR).then(function(){
+#!/usr/bin/env node
+'use strict';
+
+var split   = require('split');
+var through = require('promise-streams').through;
+
+var runner = require('../index.js');
+var Urbit = runner.Urbit;
+
+var args = process.argv.slice(2);
+var urbit = new Urbit(args);
+
+urbit.expect(runner.ERROR)
+.then(function(){
   return process.exit(1);
 });
-urbit.expect(/dojo> /).then(function(){
-  return process.stdin.pipe(split()).pipe(through(function(it){
-    return urbit.line(it.trim().replace(/\$[a-zA-Z0-9_]+/g, function(it){
-      var ref$;
-      return (ref$ = process.env[it.slice(1)]) != null ? ref$ : '__unknown-var__';
-    }));
-  })).wait().then(function(){
+
+urbit.expect(/dojo> /)
+.then(function(){
+  return process.stdin
+  .pipe(split())
+  .pipe(through(function(arg){
+    // environment variable or value
+    var val = arg.trim()
+      .replace(/\$[a-zA-Z0-9_]+/g, function(match){
+        var env = process.env[match.slice(1)];
+        return ( env != null ) ? env : '__unknown-var__';
+      });
+
+    return urbit.line(val);
+  }))
+  .wait()
+  .then(function(){
     return urbit.exit(0);
   });
 });
